e00d1ecdc96b4408853f0cb9dd8216274f1de42a
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-12 14:46:35 -0500

    finished MVP

--- src/App.js
+++ src/App.js
@@ -29,12 +29,12 @@ const [awayScore, setAwayScore] = useState(0);
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 6)}>Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick ={() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setAwayScore(AwayScore + 6)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick ={() => setAwayScore(AwayScore + 3)}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick ={() => setAwayScore(awayScore + 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
